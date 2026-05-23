c348f358d9328daa9c3c8875f6af310dcb71cc71
Jered Wierzbicki <jeredw@gmail.com>
2026-05-22 17:47:28 -0700

    Arrows and scrolling

--- gbforth.asm
+++ gbforth.asm
@@ -7,18 +7,34 @@ FrameCounter: db
 KeyTimer: db
 CurButtons: db
 NewButtons: db
+CursorX: db
+CursorY: db
 CursorPtr: dw
+
+; Control codes
+def CR equ 13
+def UP equ 24
+def DN equ 25
+def RT equ 26
+def LT equ 27
+def SC equ 32
 
 ; Keyboard geometry
 def KEYBOARD_TOP   equ SCREEN_HEIGHT_PX - 8 * 4
 def PICKER_START_Y equ 16 + KEYBOARD_TOP
 def PICKER_END_Y   equ PICKER_START_Y + 4 * 8
 def PICKER_START_X equ 7
-def PICKER_END_X   equ PICKER_START_X + 8 * 20
+def PICKER_END_X   equ PICKER_START_X + 8 * SCREEN_WIDTH
 
 ; Key repeat params
 def KEY_REPEAT_RESET equ $10  ; frames before first repeat
 def KEY_REPEAT_RATE  equ $4   ; frames between repeats
+
+; Use up to 31 columns and scroll when we hit the last column, so that we don"t
+; wrap around and show the first character offscreen to the right.
+def MAX_COLUMN    equ TILEMAP_WIDTH - 1
+def SCROLL_COLUMN equ SCREEN_WIDTH - 1
+def SCROLL_ROW    equ SCREEN_HEIGHT - 5
 
 SECTION "Top of stack", WRAMX[$DFFF], BANK[1]
 TopOfStack: ds 0
@@ -57,7 +73,11 @@ Boot:
   ld [CurButtons], a
   ld [NewButtons], a
   ld [FrameCounter], a
-  ld a, 32
+  ld [CursorX], a
+  ld [KeyTimer], a
+  ld a, 1
+  ld [CursorY], a
+  ld a, SC
   ld [CurChar], a
   ld a, LOW($9820)
   ld [CursorPtr], a
@@ -85,7 +105,7 @@ Boot:
   ; Clear tilemaps for text entry and for window
   ld hl, TILEMAP0
   ld bc, 2 * (TILEMAP1 - TILEMAP0)
-  ld d, 32
+  ld d, SC
   call FillMemory
 
   ; Install OAM DMA routine in HRAM
@@ -167,7 +187,7 @@ Main:
   jr c, .backspace
   rla               ; a
   jr c, .pick_char
-  jr .draw_cursor
+  jp .draw_cursor
 .picker_down:
   ld a, [PickerY]
   add a, 8
@@ -205,10 +225,10 @@ Main:
   ld [PickerX], a
   jp .draw_cursor
 .backspace
-  ld a, 32
+  ld a, SC
   ld [CurChar], a
-  ld de, -1
-  call MoveCursor
+.left
+  call CursorBack
   jp .draw_cursor
 .pick_char:
   ld a, [PickerY]
@@ -225,9 +245,32 @@ Main:
   ld l, a
   ld h, HIGH(TILEMAP1)
   ld a, [hl]        ; get char from tilemap
-  ld de, 1
+  cp a, CR          ; newline
+  jr z, .newline
+  cp a, DN
+  jr z, .down
+  cp a, UP
+  jr z, .up
+  cp a, RT
+  jr z, .right
+  cp a, LT
+  jr z, .left
+  jr .advance
+.newline:
+  ld a, SC
   ld [CurChar], a
-  call MoveCursor
+  call NewLine
+  jr .draw_cursor
+.down
+  call CursorDown
+  jr .draw_cursor
+.up
+  call CursorUp
+  jr .draw_cursor
+.advance
+  ld [CurChar], a
+.right
+  call CursorAdvance
 .draw_cursor
   ld b, 219         ; Show cursor on 1/4 of frames
   ld a, [FrameCounter]
@@ -252,15 +295,62 @@ PutChar:
   ld [hl], a
   ret
 
+CursorBack:
+  ld a, [CursorX]
+  cp a, 0
+  jr z, .no_back
+  dec a
+  ld [CursorX], a
+.no_back
+  jp MoveCursor
+
+CursorAdvance:
+  ld a, [CursorX]
+  inc a
+  cp a, MAX_COLUMN
+  jr z, NewLine
+  ld [CursorX], a
+  jp MoveCursor
+
+CursorUp:
+  ld a, [CursorY]
+  cp a, 0
+  jr z, UpDownStoreY
+  dec a
+  jr UpDownStoreY
+
+NewLine:
+  ld a, 0
+  ld [CursorX], a
+CursorDown:
+  ld a, [CursorY]
+  inc a
+  cp a, TILEMAP_HEIGHT
+  jr nz, UpDownStoreY
+  ld a, TILEMAP_HEIGHT - 1
+UpDownStoreY:
+  ld [CursorY], a
+  ;jp MoveCursor
+
 ; Adjust cursor position
 MoveCursor:
   ld a, [CurChar]
   ld b, a
   call PutChar
-  ld hl, CursorPtr
-  ld a, [hli]
-  ld h, [hl]
-  ld l, a
+  xor a             ; clear a and carry
+  ld e, a           ; clear e
+  ld a, [CursorY]
+  ld d, a           ; de = 256 * y
+  rr d
+  rr e
+  rr d
+  rr e
+  rr d
+  rr e              ; de = 32 * y
+  ld a, [CursorX]
+  or a, e
+  ld e, a           ; de = 32 * y + x
+  ld hl, TILEMAP0
   add hl, de
   ld a, l
   ld [CursorPtr], a
@@ -269,6 +359,26 @@ MoveCursor:
   ; Buffer character from new screen pos
   ld a, [hl]
   ld [CurChar], a
+  ; Scroll viewport left and right if needed
+  ld a, [CursorX]
+  sub a, SCROLL_COLUMN
+  jr nc, .scroll_x
+  xor a
+.scroll_x
+  rla               ; note carry is clear
+  rla
+  rla               ; rSCX = 8 * x
+  ld [rSCX], a
+  ; Scroll viewport up and down if needed
+  ld a, [CursorY]
+  sub a, SCROLL_ROW
+  jr nc, .scroll_y
+  xor a
+.scroll_y
+  rla
+  rla 
+  rla               ; rSCY = 8 * y
+  ld [rSCY], a
   ret
 
 ; Copy d to bc bytes at hl
@@ -347,16 +457,15 @@ UpdateButtons:
   ret
 
 Message:
-  db "gbforth 1.0"
+  db "\\gbforth 1.0"
 .End
 
 OnScreenKeyboard:
   ; Pad rows to 32 bytes to match VRAM tilemap width.
-  ;   01234567890123456789012345678901
-  db "1234567890 !\"#$%&'()            "
-  db "QWERTYUIOP *+,-./:;<            "
-  db "ASDFGHJKL  =>?@[\\]^_            "
-  db "ZXCVBNM  \r  `\{|\}~                "
+  db "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " ", "!", "\"", "#",  "$", "%",  "&", "'", "(", ")", "            "
+  db "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", " ", "*",  "+", ",",  "-", ".",  "/", ":", ";", "<", "            "
+  db "A", "S", "D", "F", "G", "H", "J", "K", "L",  24,  13, "=",  ">", "?",  "@", "[", "\\", "]", "^", "_", "            "
+  db "Z", "X", "C", "V", "B", "N", "M",  27,  26,  25, " ", "`", "\{", "|", "\}", "~",  " ", " ", " ", " ", "            "
 .End
 
 SECTION "Tile data", ROM0
